8325fe279800fe603f853a869c2ac55ad5d5f622
Kseniya Lukoshkina <xvluk@ya.ru>
2020-10-02 04:35:07 +0300

    play/stop button

--- testMaps/ViewController.swift
+++ testMaps/ViewController.swift
@@ -13,19 +13,62 @@ class ViewController: UIViewController {
     
     private var locationManager: CLLocationManager?
     
+    @IBOutlet weak var playImage: UIImageView!
+    @IBOutlet weak var stopImage: UIImageView!
+    
+    var isRunning: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureLocationManager()
         locationManager?.delegate = self
+        
+        updateButton()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateButton()
+    }
+    
+    private func updateButton() {
+        if isRunning {
+            playImage.isHidden = true
+            stopImage.isHidden = false
+        } else {
+            playImage.isHidden = false
+            stopImage.isHidden = true
+        }
     }
 
+    @IBAction func clickButton(_ sender: Any) {
+        if !isRunning {
+            start()
+        } else {
+            stop()
+        }
+        isRunning = !isRunning
+        updateButton()
+    }
+    
+    private func start() {
+        locationManager?.startUpdatingLocation()
+    }
+    
+    private func stop() {
+        locationManager?.stopUpdatingLocation()
+    }
     
     private func configureLocationManager() {
         locationManager = CLLocationManager()
-        locationManager?.requestWhenInUseAuthorization()
         locationManager?.delegate = self
-        locationManager?.startUpdatingLocation()
+//        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.allowsBackgroundLocationUpdates = true
+        locationManager?.pausesLocationUpdatesAutomatically = false
+        locationManager?.startMonitoringSignificantLocationChanges()
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.requestAlwaysAuthorization()
+//        locationManager?.startUpdatingLocation()
     }
     
     private func addMarker(coordinate: CLLocationCoordinate2D) {
